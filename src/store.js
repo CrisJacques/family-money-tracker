ee8ca772b373133fd8f0c2a8e6a29ec9ec66b3dd
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-05-21 17:26:51 -0300

    Desfazendo mudança desnecessária

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
+
+const middleware = [thunk];
 
 /**
  * Criando a store do Redux, passando todos os reducers criados
  */
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
