41e0795704b50350bd07d9f731b0173376529b80
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-05-21 17:09:27 -0300

    Alterações para fazer deploy no Heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Construindo um pequeno servidor de arquivos usando Node.js + Express que entregará o conteúdo estático do ReactJS a cada requisição à aplicação na Heroku
+ */
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+/**
+ * Este servidor está entregando arquivos da pasta dist, que é a pasta onde fica o build da aplicação depois de compilada
+ */
+app.use(express.static(path.join(__dirname, "dist")));
+
+/**
+ * Uso de uma variável de ambiente para a porta, esta é uma exigência da Heroku, pois lá as portas são dinâmicas
+ */
+app.set("port", process.env.PORT || 3000);
+
+const server = app.listen(app.get("port"), function () {
+  console.log("listening on port ", server.address().port);
+});
--- src/store.js
+++ src/store.js
@@ -1,17 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 
 import rootReducer from "./reducers";
-
-const middleware = [thunk];
 
 /**
  * Criando a store do Redux, passando todos os reducers criados
  */
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer);
 
 export default store;
