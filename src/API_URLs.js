6c9aac67bdce1f621f6389c598fac9cdfd13a2a5
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-09-11 19:20:54 -0300

    Ajustes nas URLs para rodar o projeto localmente e retomar os desenvolvimentos

--- src/API_URLs.js
+++ src/API_URLs.js
@@ -3,21 +3,21 @@
 /**
  * Endereço para autenticação - local
  */
-//export const API_URL_AUTH = "http://localhost:8080/api/auth/";
+export const API_URL_AUTH = "http://localhost:8080/api/auth/";
 
 /**
  * Endereço para autenticação - Heroku
  */
-export const API_URL_AUTH =
-  "https://family-money-tracker-api.herokuapp.com/api/auth/";
+//export const API_URL_AUTH =
+  //"https://family-money-tracker-api.herokuapp.com/api/auth/";
 
 /**
  * Endereço para demais operações - local
  */
-//export const API_URL_BASE = "http://localhost:8080/api/";
+export const API_URL_BASE = "http://localhost:8080/api/";
 
 /**
  * Endereço para demais operações - Heroku
  */
-export const API_URL_BASE =
-  "https://family-money-tracker-api.herokuapp.com/api/";
+//export const API_URL_BASE =
+  //"https://family-money-tracker-api.herokuapp.com/api/";
